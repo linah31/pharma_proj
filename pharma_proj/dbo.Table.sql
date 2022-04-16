fc7ee70a79f4089d3509491ab1384a3a41de6848
linah31 <20193017@fue.edu.eg>
2022-04-16 02:25:41 +0200

    DBupdate

---
+++ pharma_proj/dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[customer] (
+    [fname]    NVARCHAR (20) NOT NULL,
+    [lname]    NVARCHAR (20) NOT NULL,
+    [gender]   NCHAR (1)     NOT NULL,
+    [email]    NVARCHAR (50) NOT NULL,
+    [phone_no] NVARCHAR (20) NULL,
+    [country]  NVARCHAR (20) NOT NULL,
+    [username] NVARCHAR (20) NOT NULL,
+    [password] NVARCHAR (20) NOT NULL,
+    PRIMARY KEY CLUSTERED ([password] ASC),
+    CONSTRAINT [CK_Table_Column] CHECK ([gender]='M' OR [gender]='F')
+);
+
