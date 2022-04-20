7ccfca813ebe8b6ab24671e4a86a24a2ae35936c
linah <20193017@fue.edu.eg>
2022-04-20 10:26:46 +0200

    DBUPDATE2

--- pharma_proj/signup.aspx.cs
+++ pharma_proj/signup.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
 
 namespace pharma_proj
 {
@@ -16,9 +18,26 @@ namespace pharma_proj
 
         protected void btnsubmit_Click(object sender, EventArgs e)
         {
+            SqlConnection c = new SqlConnection();
+            c.ConnectionString= "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|pharma.mdf;Integrated Security=True";
 
-            lmsg.Text = "you are signed in !!";
-            
+            // 2- Create SQL Insert Statement string
+            string strInsert = String.Format("INSERT INTO customer VALUES('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}')", txtfname.Text, txtLname.Text, rbgender.SelectedValue, txtemail.Text, txtphone.Text, ddcountry.SelectedValue, txtUname.Text, txtp.Text);
+
+            // 3- Create SQL Command
+            SqlCommand cmdInsert = new SqlCommand(strInsert, c);
+
+            // 4- Open the database by the connection object conn
+            c.Open();
+
+            // 5- Execute the SQL command cmdInsert
+            cmdInsert.ExecuteNonQuery();
+
+            // 6- Close the database
+            c.Close();
+
+            // lmsg.Text = "you are signed in !!";
+
         }
     }
 }
